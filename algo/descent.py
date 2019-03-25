8938c4b29972439ec8f520158ffc131c48ec1b80
Nick Teff <nicholas.teff@gmail.com>
2019-03-25 16:41:40 -0500

    Minor fix -- checks through n = 9

--- algo/descent.py
+++ algo/descent.py
@@ -1,7 +1,7 @@
 from bisect import bisect_right
 import numpy as np
 from sympy.combinatorics import Permutation
-from algo import descents, RSK
+from algo import descents, desc, RSK
 from algo import RSK
 
 def max_descent(p):
@@ -13,32 +13,6 @@ def index_max_descent(p):
 
 def find_valleys(p):
     return [i for i in range(1,len(p)-1) if p[i-1] > p[i] < p[i+1]]
-
-def rolling(p):
-    if len(find_valleys(p)) > 0:
-        v = min(find_valleys(p))
-        if len(descents(p[v:])) > 0:
-            m = max_descent(p[v:])
-        else:
-m
-def descent_algo(p):
-    pp = p
-    i = 0
-    while pp != sorted(pp):
-
-
-
-"""lemma:  If m is the maximum descent and there exists M > m with w_i = m and w_j = N where i < j, then m+1,m+2,...,n are all to the right of m and appear in consecutive order.
-
-pf. if any appear before m, then there must be a descent to get down to m since m is a descent, so none can be before.  Therefore they are all after, again if they are not in consecutive order then a descent must appear to get to all of them."""
-
-
-def remove(p, q):
-    return [i for i in p if i not in q]
-def sequence(a, p, i):
-    for j in p:
-        a[j-1] = i
-    return a
 
 def remover(pp):
     mn = index_max_descent(pp)
@@ -58,8 +32,8 @@ def remover(pp):
         v = min(valleys)
         des_v = descents(pq[v:])
 
-        if len(des_v) > 0:
-            return remover(pq, a, i)
+        if des_v != []:
+            return remover(pq[v:])
         else:
             climbs = [j for j in pq[v:] if bisect_right(pq[v-1::-1], j) != 0]
             if climbs == []:
@@ -69,9 +43,31 @@ def remover(pp):
                 return [i for i in pq if (i < j)]
 
 
+def remove(p, q):
+    return [i for i in p if i not in q]
 
-p = [4,2,1,3]
-pp = p
-i = 0
-a = [0 for i in sorted(p)]
-remover(p)
+def sequence(a, p, i):
+    for j in p:
+        a[j-1] = i
+    return a
+
+def descent_algo(p):
+    pp = p
+    i = 0
+    a = [0 for i in sorted(p)]
+    deg = {i:0 for i in range(2*len(p))}
+    while pp != sorted(pp):
+        pq = remover(pp)
+        d = desc(pq)
+        i += 1
+        deg[i] = d
+        pp = remove(pp, pq)
+        a = sequence(a, pq, i)
+    deg = {i:deg[i] for i in deg.keys() if deg[i] != 0}
+    return a, deg
+
+
+
+"""lemma:  If m is the maximum descent and there exists M > m with w_i = m and w_j = N where i < j, then m+1,m+2,...,n are all to the right of m and appear in consecutive order.
+
+pf. if any appear before m, then there must be a descent to get down to m since m is a descent, so none can be before.  Therefore they are all after, again if they are not in consecutive order then a descent must appear to get to all of them."""
